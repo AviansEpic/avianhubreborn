088869b8230fc9e866b6c9220bc1bd283f2d0415
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2021-09-26 09:29:24 -0600

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 1962086868 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AviansEpic/avianhubreborn/main/toh.lua"))()
+end
+
+print("Thanks for using Avianhub Reborn!")
