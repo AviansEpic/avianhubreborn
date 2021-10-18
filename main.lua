2e7264a145e4efcaa8089fbe2d3ecf0dcec5d2cd
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2021-10-17 18:39:31 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 1962086868 then
-  loadstring(game:HttpGet(("https://raw.githubusercontent.com/AviansEpic/avianhubreborn/main/toh.lua"), true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AviansEpic/avianhubreborn/main/toh.lua", true))()
 else if game.PlaceId == 4924922222 then
-  loadstring(game:HttpGet(("https://raw.githubusercontent.com/AviansEpic/avianhubreborn/main/brookhaven.lua"), true))()
-end
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AviansEpic/avianhubreborn/main/brookhaven.lua", true))()
+ end
 
 print("Thanks for using Avianhub Reborn!")
